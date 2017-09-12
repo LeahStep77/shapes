e716c95245c37f75cc6f0e11a2de10344b0c7819
LeahStep77 <fanaticalbotanical@yahoo.com>
2017-09-12 14:27:15 -0500

    updating shape.js

--- shape.js
+++ shape.js
@@ -1,42 +1,57 @@
-function Shape(type) {
-	this.type = type;
+function Shape(type, sides) {
+	this.type = type,
+	args = Array.prototype.slice.call(arguments, 1);
+  	this.sides = args.join(" X ");
 	}    
 
 
 Shape.prototype.thisType = function thisType (){
-	return "this is a " + this.type 
+	return "this is a " + this.type + "it measures " + this.sides 
 };
 
-var triangle = new Shape("triangle")
-
+var triangle = new Shape("triangle", 7, 10, 20);
 var aShape =  triangle.thisType();
-
+console.log(aShape);
 console.log (triangle instanceof Shape);
 
-function AnyShape (side1, side2, side3, side4){
-	this.side1 = side1;
-	this.side2 = side2;
-	this.side3 = side3;	
-	this.side4 = side4;
-}
+var square = new Shape("square", 8, 8, 8, 8);
+var aSq = square.thisType();
+console.log(aSq);
+console.log(square instanceof Shape);
 
-AnyShape.prototype.thisAnyShape = function thisAnyShape(){
-	return "sides equal "+this.side1
-}
 
-var secTri = new AnyShape (2,4,6)
+// function AnyShape (sides) {
+//   var args = Array.prototype.slice.call(arguments);
+//   this.sides = args.join(" X ");
+// }
 
-var aTri = secTri.thisAnyShape();
 
-console.log(aShape + " " +aTri);
+// AnyShape.prototype.thisAnyShape = function thisAnyShape(){
+// 	return "sides equal "+this.sides
+// }
+
+// var secTri = new AnyShape (2,4,6)
+
+// var aTri = secTri.thisAnyShape();
+
+// console.log(aShape + " " +aTri);
 
  
-function Square (side1){
-	this.side1 = side1;
-}
+// function Square (side1){
+// 	this.side1 = side1;
+// }
 
-var square = new Shape("square");
-var aShape1 =  square.thisType();
-var secSq = new AnyShape (7);
-var aSq = secSq.thisAnyShape();
-console.log(aShape1 + " " +aSq);
+// var square = new Shape("square");
+// var aShape1 =  square.thisType();
+// var secSq = new AnyShape (7, 7, 7, 7);
+// var aSq = secSq.thisAnyShape();
+// console.log(aShape1 + " " +aSq);
+
+////////////////////////////////
+// function afunction (sep) {
+//   var args = Array.prototype.slice.call(arguments);
+//   return args.join(" X ");
+// }
+// console.log(afunction("apple","pineapple","avacado"));
+
+
